7f96d494eb7317b067e14833f1c8c896a5744a89
Berend Romijn <berend2002@gmail.com>
2026-03-17 14:42:50 +0100

    made use cases pages

--- front-end/app.py
+++ front-end/app.py
@@ -7,5 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', cards=cards)
 
+@app.route('/postnl')
+def postnl():
+    return render_template('use_cases/postnl.html')
+
+
+@app.route('/pharmaceutical')
+def pharmaceutical():
+    return render_template('use_cases/pharmaceutical.html')
+
+
+@app.route('/hive-zox')
+def hive_zox():
+    return render_template('use_cases/Hive-zox.html')
+
+
+@app.route('/fixed-wireless-access')
+def fixed_wireless_access():
+    return render_template('use_cases/fixed-wireless-access.html')
+
+
+@app.route('/logistics-security')
+def logistics_security():
+    return render_template('use_cases/logistics-security.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
